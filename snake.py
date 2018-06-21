6aec648cda54e106373cbc30ab6524ec7eb921da
lukqw <lukqw@users.noreply.github.com>
2018-06-22 00:05:18 +0200

    cleanup of snake

--- snake.py
+++ snake.py
@@ -11,36 +11,36 @@ class Snake:
         self.apple.create()
 
     def up(self):
+        x = self.xy[0][0]
         y = self.xy[0][1]
         if y != 0:
-            y -= 1
+            self.move(x, y-1)
         else:
-            y = self.field - 1
-        self.move(self.xy[0][0], y)
+            self.move(x, self.field-1)
 
     def down(self):
+        x = self.xy[0][0]
         y = self.xy[0][1]
         if y != self.field - 1:
-            y += 1
+            self.move(x, y + 1)
         else:
-            y = 0
-        self.move(self.xy[0][0], y)
+            self.move(x, 0)
 
     def left(self):
         x = self.xy[0][0]
+        y = self.xy[0][1]
         if x != 0:
-            x -= 1
+            self.move(x - 1, y)
         else:
-            x = self.field - 1
-        self.move(x, self.xy[0][1])
+            self.move(self.field - 1, y)
 
     def right(self):
         x = self.xy[0][0]
+        y = self.xy[0][1]
         if x != self.field - 1:
-            x += 1
+            self.move(x + 1, y)
         else:
-            x = 0
-        self.move(x, self.xy[0][1])
+            self.move(0, y)
 
     def move(self, x, y):
         if (x, y) == (self.apple.x, self.apple.y):
@@ -48,8 +48,14 @@ class Snake:
         elif (x, y) in self.xy:
             self.sense.show_message("You lose!", scroll_speed=0.05)
         else:
-            self.sense.set_pixel(self.xy[-1][0], self.xy[-1][1], 0, 0, 0)
-            self.xy.remove(self.xy[-1])
+            self.remove_last()
+        self.add_head(x, y)
+
+    def remove_last(self):
+        self.sense.set_pixel(self.xy[-1][0], self.xy[-1][1], 0, 0, 0)
+        self.xy.remove(self.xy[-1])
+
+    def add_head(self, x, y):
         self.xy.insert(0, (x, y))
         self.sense.set_pixel(x, y, self.rgb)
 
